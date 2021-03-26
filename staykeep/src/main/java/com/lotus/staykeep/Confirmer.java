b6cacb1c746f4d8e947e8327d89ddedb4d2225a9
Josh Cushing <joshua.cushing@revature.net>
2021-03-26 15:14:40 -0400

    Made Staykeep the SOAP server for my project

---
+++ staykeep/src/main/java/com/lotus/staykeep/Confirmer.java
@@ -0,0 +1,10 @@
+package com.lotus.staykeep;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService
+public interface Confirmer {
+	@WebMethod
+	String confirm();
+}
---
+++ staykeep/src/main/java/com/lotus/staykeep/ConfirmerImpl.java
@@ -0,0 +1,13 @@
+package com.lotus.staykeep;
+
+import javax.jws.WebService;
+
+@WebService(endpointInterface = "com.lotus.staykeep.Confirmer", 
+serviceName ="confirmerImplService")
+public class ConfirmerImpl implements Confirmer {
+
+	public String confirm() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
